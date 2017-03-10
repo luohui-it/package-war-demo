bf11686a198517e57dc3cafeea0e97bedeab0570
luohui <525078676@qq.com>
2017-03-10 18:10:18 +0800

    打war包的demo

---
+++ module-package/src/main/java/com/bestpay/demo/common/DemoTest.java
@@ -0,0 +1,11 @@
+package com.bestpay.demo.common;
+
+/**
+ * 类注释
+ * <p>
+ * </p>
+ * User: ThinkPad Date: 2016/05/04 ProjectName: package-demo Version: 1.0
+ */
+
+public class DemoTest {
+}
---
+++ module-web/src/test/java/Test.java
@@ -0,0 +1,15 @@
+/**
+ * 类注释
+ * <p>
+ * </p>
+ * User: ThinkPad Date: 2016/06/22 ProjectName: package-demo Version: 1.0
+ */
+
+public class Test {
+
+    public static void main(String[] args){
+        String str = "汉字咯a";
+        byte[] a = str.getBytes();
+        System.out.println(a.length);
+    }
+}
